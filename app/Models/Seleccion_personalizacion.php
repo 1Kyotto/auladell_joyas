cbf180af56dd650b1effa5538c73d7dd4bf0e465
Guilmon-Kyo <martin.gonzalez.nunez@gmail.com>
2024-08-22 17:07:11 -0400

    Implementacion de modelos

---
+++ app/Models/Seleccion_personalizacion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Seleccion_personalizacion extends Model
+{
+    use HasFactory;
+}
